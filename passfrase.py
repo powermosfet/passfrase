254bdf192d20a7350f42a4cbf26b7cb14224abaa
Asmund Berge <herrasbe@gmail.com>
2015-10-29 11:06:02 +0100

    Converting to Python 3 because unicode

--- passfrase.py
+++ passfrase.py
@@ -5,18 +5,18 @@ def passfrase(filename, n_words, sep, alphabet, min, max):
     words = [ w.strip() for w in words ]
     words = [ w for w in words if len(w) >= min and len(w) <= max ]
     if not words:
-        print "ERROR: No words left"
+        print("ERROR: No words left")
         return
     phrasewords = [ random.choice(words) for x in range(n_words) ]
-    passfrasestring = unicode(sep).join(phrasewords)
-    print passfrasestring
-    print 
-    print "Entropy:"
-    print "If method and dictionary is known: {} bits".format(
-            int(math.log(len(words)**n_words, 2)))
-    print "Unknown method, alphabet size {}: {} bits".format(
+    passfrasestring = sep.join(phrasewords)
+    print(passfrasestring)
+    print()
+    print("Entropy:")
+    print("If method and dictionary is known: {} bits".format(
+            int(math.log(len(words)**n_words, 2))))
+    print("Unknown method, alphabet size {}: {} bits".format(
             alphabet, 
-            int(math.log(alphabet**len(passfrasestring), 2)))
+            int(math.log(alphabet**len(passfrasestring), 2))))
 
 parser = argparse.ArgumentParser()
 parser.add_argument("filename", help="Dictionary file. One word per line")
