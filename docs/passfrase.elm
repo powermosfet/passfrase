d08f1fa1ff7c6cb6e9978d181f711c69a420724b
Asmund Berge <herrasbe@gmail.com>
2016-12-01 09:50:00 +0100

    Use range for number og words

--- docs/passfrase.elm
+++ docs/passfrase.elm
@@ -26,12 +26,12 @@ update msg model =
 
 -- VIEW
 view : Model -> Html Msg
-view model =
+view model = 
   div []
     [ h1 [] [ text "Passfrase" ]
     , div [ class "form-group col-md-3" ] 
       [ label [ for "inputNumberOfWords" ] [ text "Antall ord" ]
-      , input [ type_ "number", class "form-control", id "inputNumberOfWords", value "4" ] []
+      , input [ type_ "range", class "form-control", id "inputNumberOfWords", value "4" ] []
       ]
     , div [ class "form-group col-md-9" ] 
       [ label [ for "inputNumberOfWords" ] [ text "Innstillinger" ]
@@ -50,7 +50,7 @@ view model =
       ]
     , div [ class "panel panel-default passphrase-panel col-md-12" ]
         [ div [ class "panel-body" ]
-          [ h1 [ class "passphrase-text" ] [ text "Banan Klatretau Viskelær Jungel ¤4" ]
+          [ h1 [ class "passphrase-text" ] [ text "> Banan Klatretau Viskelær Jungel ¤4" ]
           ]
         ]
     ]
